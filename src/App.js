a42019136dc69c8016c26f27747d1629f9002240
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-23 16:06:47 +0600

    design navbar and banner

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Header from './Page/Shared/Header/Header';
 
 function App() {
   return (
-    <div className='max-w-7xl mx-auto'>
+    <div>
+      {/* className='max-w-7xl mx-auto' */}
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     themes: [
       {
         mytheme: {
-          primary: "#a991f7",
+          primary: "#FF5E14",
           secondary: "#f6d860",
           accent: "#37cdbe",
           neutral: "#3d4451",
