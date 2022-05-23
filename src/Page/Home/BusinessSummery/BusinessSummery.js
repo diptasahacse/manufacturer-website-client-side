e6740d21ae32f61a922aa30b90ba08ba43357ee6
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-23 23:26:27 +0600

    business summery section design complete

---
+++ src/Page/Home/BusinessSummery/BusinessSummery.js
@@ -0,0 +1,49 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import CustomTitle from '../../Shared/CustomTitle/CustomTitle';
+import { faUsers, faHeart, faScrewdriverWrench } from '@fortawesome/free-solid-svg-icons';
+const BusinessSummery = () => {
+    return (
+        <div class="hero" style={{ backgroundImage: "url(https://i.ibb.co/KDzQ6y8/counter-bg.jpg)" }}>
+            <div class="hero-overlay bg-opacity-90"></div>
+            <div class="hero-content text-center text-neutral-content">
+                <div className='py-9 px-3 max-w-7xl mx-auto'>
+                    <CustomTitle title='Business Summery'></CustomTitle>
+
+                    <div className='grid mt-7 grid-cols-3 gap-36'>
+                        <div className='flex items-center'>
+                            <span className='text-6xl text-secondary '>
+                                <FontAwesomeIcon icon={faUsers} />
+                            </span>
+                            <div className='ml-5'>
+                                <p className='font-bold text-5xl text-white'>273+</p>
+                                <p className='text-left text-2xl text-secondary'>Users</p>
+                            </div>
+                        </div>
+                        <div className='flex items-center'>
+                            <span className='text-6xl text-secondary '>
+                                <FontAwesomeIcon icon={faScrewdriverWrench} />
+                            </span>
+                            <div className='ml-5'>
+                                <p className='font-bold text-5xl text-white'>273+</p>
+                                <p className='text-left text-2xl text-secondary'>Tools</p>
+                            </div>
+                        </div>
+                        <div className='flex items-center'>
+                            <span className='text-6xl text-secondary '>
+                                <FontAwesomeIcon icon={faHeart} />
+                            </span>
+                            <div className='ml-5'>
+                                <p className='font-bold text-5xl text-white'>273+</p>
+                                <p className='text-left text-2xl text-secondary'>Feedback</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BusinessSummery;
--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import BusinessSummery from './BusinessSummery/BusinessSummery';
 import ToolsSection from './ToolsSection/ToolsSection';
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <ToolsSection></ToolsSection>
+            <BusinessSummery></BusinessSummery>
         </div>
     );
 };
