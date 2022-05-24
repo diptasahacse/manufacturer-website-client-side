b078a90dfb3ae2b6db9e5304b43b9cefc3582048
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-24 17:13:51 +0600

    nav link in dropdown when visit in mobile in navbar

--- src/Page/Shared/Header/Header.js
+++ src/Page/Shared/Header/Header.js
@@ -12,8 +12,8 @@ const Header = () => {
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                             </label>
                             <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                                <li><a>Item 1</a></li>
-                                <li><a>Item 3</a></li>
+                                <li><Link to='/'>Home</Link></li>
+                                <li><Link to='/signin'>Sign In</Link></li>
                             </ul>
                         </div>
                         <Link to='/' className='btn btn-ghost text-primary normal-case text-2xl'>useTools</Link>
@@ -22,7 +22,7 @@ const Header = () => {
                         <ul class="menu menu-horizontal p-0">
                             <li><Link to='/'>Home</Link></li>
                             <li><Link to='/signin'>Sign In</Link></li>
-                            
+
                         </ul>
                     </div>
 
