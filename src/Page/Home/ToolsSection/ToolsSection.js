55d35898bb5e5b7e3d8b88564223aec960fbbea9
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-24 12:59:21 +0600

    design loading component

--- src/Page/Home/ToolsSection/ToolsSection.js
+++ src/Page/Home/ToolsSection/ToolsSection.js
@@ -1,17 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import CustomTitle from '../../Shared/CustomTitle/CustomTitle';
+import Loading from '../../Shared/Loading/Loading';
 import ProductsCard from './ProductsCard/ProductsCard';
 
 const ToolsSection = () => {
     const [allTools, setAllTools] = useState([])
+    const [isLoading, setIsLoading] = useState(false);
     useEffect(() => {
+        setIsLoading(true)
         fetch('https://infinite-chamber-43931.herokuapp.com/products')
             .then(res => res.json())
-            .then(data => setAllTools(data))
+            .then(data => {
+
+                setAllTools(data)
+                setIsLoading(false)
+            })
 
     }, [])
+    if(isLoading){
+        return <Loading></Loading>
 
-    console.log(allTools)
+    }
+
+
     return (
         <div className='py-10 bg-accent'>
             <div className='max-w-7xl mx-auto'>
