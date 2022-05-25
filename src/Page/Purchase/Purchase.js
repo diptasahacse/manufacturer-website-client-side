7bbfb2626085f2e9156da1f859c9425e13d6422f
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-25 17:28:39 +0600

    user can increase or decrease quantity also show error message and purchase button according to quantity condition

--- src/Page/Purchase/Purchase.js
+++ src/Page/Purchase/Purchase.js
@@ -1,11 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading/Loading';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 const Purchase = () => {
     const { productId } = useParams();
     const [productInfo, setProductInfo] = useState({});
-    const [isLoading, setIsLoading] = useState(false)
+    const [orginalProductInfo, setOrginalProductInfo] = useState({});
+
+    const [isLoading, setIsLoading] = useState(false);
+    let quantityErrorMessage;
     useEffect(() => {
         setIsLoading(true)
         fetch(`http://localhost:5000/products/${productId}`, {
@@ -15,17 +20,40 @@ const Purchase = () => {
             .then(res => res.json())
             .then(data => {
                 setProductInfo(data);
+                setOrginalProductInfo(data)
                 setIsLoading(false)
 
             })
 
     }, [productId])
+    const onQuantityChange = (event) => {
+
+        const { minOrderQuantity, ...rest } = productInfo;
+        const newQuantity = event.target.value;
+        // if (Number(newQuantity) < orginalProductInfo.minOrderQuantity) {
+        //     // <span class="label-text-alt text-red-600">Alt label</span>
+        //     quantityErrorMessage = <span class="label-text-alt text-red-600">`You have to purchase at least ${minOrderQuantity} or more`</span>
+        // }
+        // else if (Number(newQuantity) > orginalProductInfo.availableQuantity) {
+        //     quantityErrorMessage = <span class="label-text-alt text-red-600">`You have to purchase at most ${minOrderQuantity} or less`</span>
+        // }
+
+        const newProductInfo = { minOrderQuantity: Number(newQuantity), ...rest }
+        setProductInfo(newProductInfo)
+
+
+
+
+
+    }
+
 
     if (isLoading) {
 
         return <Loading></Loading>
     }
-    console.log(productInfo)
+    // console.log(productInfo)
+    // console.log(productInfo?.minOrderQuantity)
     return (
         <div className='py-10'>
             <div className='px-5 lg:px-20 max-w-7xl mx-auto'>
@@ -57,8 +85,33 @@ const Purchase = () => {
                                 </div>
                             </div>
                             <div className='mt-10 flex items-center'>
-                                <h2 className='text-4xl font-semibold text-primary'>Total: ${productInfo?.price * productInfo?.minOrderQuantity} </h2>
+                                <h2 className='text-3xl font-semibold text-primary'>Total: ${productInfo?.price * productInfo?.minOrderQuantity} </h2>
                                 <span className='font-light text-sm ml-3'>( ${productInfo?.price} X {productInfo?.minOrderQuantity} )</span>
+
+                            </div>
+                            <div className='mt-10'>
+                                <div>
+                                    <div class="form-control w-full max-w-xs">
+                                        <label class="label">
+                                            <span class="label-text text-primary">How much you want..?</span>
+
+                                        </label>
+                                        <input onChange={onQuantityChange} type="number" value={productInfo?.minOrderQuantity} class="input input-bordered w-full max-w-xs" />
+                                        <label class="label">
+                                            {
+                                                productInfo?.minOrderQuantity < orginalProductInfo?.minOrderQuantity && <span class="label-text-alt text-red-600">You have to purchase at least {orginalProductInfo?.minOrderQuantity} or more</span>
+                                            }
+                                            {
+                                                productInfo?.minOrderQuantity > orginalProductInfo?.availableQuantity && <span class="label-text-alt text-red-600">You have to purchase at most {orginalProductInfo?.availableQuantity} or less</span>
+                                            }
+                                        </label>
+                                    </div>
+                                    <div>
+                                        {
+                                            (productInfo?.minOrderQuantity >= orginalProductInfo?.minOrderQuantity && productInfo?.minOrderQuantity <= orginalProductInfo?.availableQuantity) && <button className="btn btn-primary">Purchase Now <span className='ml-2'><FontAwesomeIcon icon={faCartShopping} /></span></button>
+                                        }
+                                    </div>
+                                </div>
 
                             </div>
                         </div>
