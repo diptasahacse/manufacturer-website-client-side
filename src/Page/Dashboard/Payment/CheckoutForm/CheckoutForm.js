412521c85033a073ddb36425d0006d0e8739495f
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-26 00:57:31 +0600

    show card error in toast from checkout page

--- src/Page/Dashboard/Payment/CheckoutForm/CheckoutForm.js
+++ src/Page/Dashboard/Payment/CheckoutForm/CheckoutForm.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     CardElement,
     Elements,
     useStripe,
     useElements,
 } from '@stripe/react-stripe-js';
+import { async } from '@firebase/util';
+import { toast } from 'react-toastify';
 const CheckoutForm = () => {
     const stripe = useStripe();
+    const elements = useElements();
+    const [cardError, setCardError] = useState('')
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault()
 
+        if (!stripe || !elements) {
+            // Stripe.js has not loaded yet. Make sure to disable
+            // form submission until Stripe.js has loaded.
+            return;
+        }
+        // Get a reference to a mounted CardElement. Elements knows how
+        // to find your CardElement because there can only ever be one of
+        // each type of element.
+        const card = elements.getElement(CardElement);
+
+        if (card == null) {
+            return;
+        }
+
+        // Use your card Element with other Stripe.js APIs
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+        if(error){
+            toast.error(error.message)
+            setCardError(error.message)
+        }
+        else{
+            setCardError("")
+        }
+        
+
+
     }
+    console.log(cardError)
 
     return (
         <form onSubmit={handleSubmit}>
