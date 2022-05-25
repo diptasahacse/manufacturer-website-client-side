664ab09617b2d3c475914f73b2cda70f5e9273d8
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-25 13:08:22 +0600

    an admin can able to remove any admin

--- src/Page/Dashboard/ManageAdmin/UserRow/UserRow.js
+++ src/Page/Dashboard/ManageAdmin/UserRow/UserRow.js
@@ -20,14 +20,14 @@ const UserRow = ({ index, singleUser, refetch }) => {
 
         })
             .then(res => {
-                console.log(res)
+                // console.log(res)
                 if (res.status === 403) {
                     toast.error('Failed to make an admin')
                 }
                 return res.json()
             })
             .then(data => {
-                console.log(data)
+                // console.log(data)
                 if (data.modifiedCount > 0) {
                     refetch()
                     toast(`Successfully made an admin for ${email}`)
@@ -37,7 +37,28 @@ const UserRow = ({ index, singleUser, refetch }) => {
             })
 
     }
-    const removeAdminHandler = () => {
+    const removeAdminHandler = (email) => {
+        fetch(`http://localhost:5000/admin/removeadmin/${email}`, {
+            method: "PUT",
+            headers: { authorization: `Bearer ${localStorage.getItem('accessToken')}` }
+
+        })
+            .then(res => {
+                console.log(res)
+                if (res.status === 403) {
+                    toast.error('Failed to remove an admin')
+                }
+                return res.json()
+            })
+            .then(data => {
+                console.log(data)
+                if (data.modifiedCount > 0) {
+                    refetch()
+                    toast(`Successfully remove an admin for ${email}`)
+
+                }
+
+            })
 
     }
 
@@ -53,7 +74,7 @@ const UserRow = ({ index, singleUser, refetch }) => {
             <th>{index + 1}</th>
             <td className={user.email === email && 'text-primary font-bold'}>{user.email === email ? email + " (You)" : email}  </td>
             <td><button onClick={() => { createAdminHandler(email) }} disabled={singleUser?.role} class="btn btn-xs btn-success">Make Admin</button></td>
-            <td><button onClick={removeAdminHandler} disabled={user.email === email || !singleUser?.role} className='btn btn-xs btn-error'>Remove Admin</button></td>
+            <td><button onClick={() => { removeAdminHandler(email) }} disabled={user.email === email || !singleUser?.role} className='btn btn-xs btn-error'>Remove Admin</button></td>
         </tr>
     );
 };
