29ff9a6898c088d3f76aeaf168c8766b4d9540f6
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-25 21:32:22 +0600

    remove dropdown profile from navbar and keep simple logout button

--- src/Page/Shared/Header/Header.js
+++ src/Page/Shared/Header/Header.js
@@ -51,15 +51,7 @@ const Header = () => {
                             <li><Link to='/dashboard'>Dashboard</Link></li>
                             {/* Start Dropdown */}
                             {user ?
-                                <li tabindex="0">
-                                    <a className='text-primary font-bold'>
-                                        {user?.displayName}
-                                        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                                    </a>
-                                    <ul class="p-2 bg-base-100">
-                                        <li><button className='text-red-500' onClick={singOutHandler}>Sign out</button></li>
-                                    </ul>
-                                </li>
+                                <li><button className='text-red-500' onClick={singOutHandler}>Sign out</button></li>
                                 :
                                 <li><Link to='/signin'>Sign In</Link></li>
                             }
