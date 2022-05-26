646fb09fcacbdd2273ac59f873676793b3749d9b
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-26 23:43:14 +0600

    design manage all orders

--- src/Page/Dashboard/ManageAllOrders/ManageAllOrders.js
+++ src/Page/Dashboard/ManageAllOrders/ManageAllOrders.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../../Shared/Loading/Loading';
 
 const ManageAllOrders = () => {
+    const { isLoading, data, refetch } = useQuery(['allOrders'], () =>
+        fetch(`http://localhost:5000/orders`, {
+            method: "GET",
+            headers: { authorization: `Bearer ${localStorage.getItem('accessToken')}` }
+
+        }).then(res =>
+            res.json()
+        )
+    )
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+    console.log(data)
     return (
         <div className='p-7 rounded-2xl' style={{ backgroundColor: "#FFFFFF" }}>
             <h3 className='text-3xl font-semibold text-primary'>Manage All Orders</h3>
+
+            <div className='mt-5'>
+                <div class="overflow-x-auto">
+                    <table class="table w-full">
+
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Job</th>
+                                <th>Favorite Color</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <tr>
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Quality Control Specialist</td>
+                                <td>Blue</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
 
         </div>
     );
