4c91b5d36e012e3f5370c28c659848e15695a55c
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-26 11:53:24 +0600

    design delete order modal

--- src/Page/Dashboard/MyOrders/OrderDeleteModal/OrderDeleteModal.js
+++ src/Page/Dashboard/MyOrders/OrderDeleteModal/OrderDeleteModal.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-const OrderDeleteModal = ({selectedOrders}) => {
-    const {productName} = selectedOrders;
-    
+const OrderDeleteModal = ({ selectedOrders }) => {
+    const { _id, productName } = selectedOrders;
+    const cancleOrderHandler = () => {
+        console.log(_id)
+
+    }
+
     return (
         <div>
             {/* <!-- Put this part before </body> tag-- > */}
@@ -10,8 +14,11 @@ const OrderDeleteModal = ({selectedOrders}) => {
             <div class="modal">
                 <div class="modal-box relative">
                     <label for="order-delete-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 class="text-lg font-bold text-error">Are you sure want to delete <span>{productName}</span> order ..?</h3>
-                    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    <h3 class="text-lg font-bold">Are you sure want to delete <span className='text-primary'>{productName}</span> order ..?</h3>
+                    <p class="py-4">If you delete this order, you can not undo it later.</p>
+                    <div className='text-right'>
+                        <button onClick={cancleOrderHandler} className='btn text-white btn-error btn-sm'>Yes</button>
+                    </div>
                 </div>
             </div>
 
