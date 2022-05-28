e6cc74823eacbce9d17606e7af2c7d48fd2ca4d3
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-28 12:42:12 +0600

    show real data in business summery

--- src/Page/Home/BusinessSummery/BusinessSummery.js
+++ src/Page/Home/BusinessSummery/BusinessSummery.js
@@ -2,13 +2,33 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import CustomTitle from '../../Shared/CustomTitle/CustomTitle';
 import { faUsers, faHeart, faScrewdriverWrench } from '@fortawesome/free-solid-svg-icons';
+import { useQuery } from 'react-query';
+import Loading from '../../Shared/Loading/Loading';
 const BusinessSummery = () => {
+
+    const { isLoading, data, refetch } = useQuery(['businessSummery'], () =>
+        fetch(`http://localhost:5000/businesssummery`, {
+            method: "GET"
+        }).then(res =>
+            res.json()
+        )
+    )
+
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
+    const { totalTools, totalUser, totalOrders } = data;
+
+
+    const orderWithReview = totalOrders.filter(order => order?.review)
     return (
         <div class="hero" style={{ backgroundImage: "url(https://i.ibb.co/KDzQ6y8/counter-bg.jpg)" }}>
             <div class="hero-overlay bg-opacity-90"></div>
             <div class="hero-content text-center text-neutral-content">
                 <div className='pt-9 pb-20 px-3 max-w-7xl mx-auto'>
-                    
+
                     <CustomTitle iconColor='#E6E6E6' titleColor='#E6E6E6' title='Business Summery'></CustomTitle>
 
                     <div className='grid mt-7 grid-cols-1 lg:grid-cols-3 gap-20 lg:gap-36'>
@@ -17,7 +37,7 @@ const BusinessSummery = () => {
                                 <FontAwesomeIcon icon={faUsers} />
                             </span>
                             <div className='ml-5'>
-                                <p className='font-bold text-5xl text-white'>273+</p>
+                                <p className='font-bold text-5xl text-white'>{totalUser}+</p>
                                 <p className='text-left text-2xl text-secondary'>Users</p>
                             </div>
                         </div>
@@ -26,7 +46,7 @@ const BusinessSummery = () => {
                                 <FontAwesomeIcon icon={faScrewdriverWrench} />
                             </span>
                             <div className='ml-5'>
-                                <p className='font-bold text-5xl text-white'>273+</p>
+                                <p className='font-bold text-5xl text-white'>{totalTools}+</p>
                                 <p className='text-left text-2xl text-secondary'>Tools</p>
                             </div>
                         </div>
@@ -35,7 +55,7 @@ const BusinessSummery = () => {
                                 <FontAwesomeIcon icon={faHeart} />
                             </span>
                             <div className='ml-5'>
-                                <p className='font-bold text-5xl text-white'>273+</p>
+                                <p className='font-bold text-5xl text-white'>{orderWithReview.length}+</p>
                                 <p className='text-left text-2xl text-secondary'>Feedback</p>
                             </div>
                         </div>
